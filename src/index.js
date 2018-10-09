4b6d97d5d3bbf26d6a05ec930cc7ffe27051e69e
elrohil <wieslaw.stanek@spinney.io>
2018-10-09 21:57:14 +0200

    Testing firmware package updating

--- src/index.js
+++ src/index.js
@@ -4,16 +4,26 @@ const server = LwM2M.createServer({
   type: 'udp4',
 });
 
+const javascriptCode = `function xpy(x, y) {
+  const xm1 = x - 1;
+  const yp1 = y + 1;
+  return xm1 + yp1;
+}`;
+
 server.on('register', (params, accept) => {
   setImmediate(() => {
+    server
+      .write(params.ep, '5/0/0', javascriptCode)
+      .then(console.log)
+      .catch(console.log);
     server
       .read(params.ep, '3/0')
       .then((device) => {
         console.log(JSON.stringify(device, null, 4));
       });
-    accept();
   });
+  accept();
 });
 
-console.log('Server listening on port 5678');
-server.listen(5678);
+console.log('Server listening on port 5683');
+server.listen(5683);
